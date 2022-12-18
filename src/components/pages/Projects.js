276cc0c350ea3d49ae9f6057f0437978216e1dc5
Demian143 <patrickxavier140@gmail.com>
2022-12-18 14:59:19 -0300

    Aula 26

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,6 +1,9 @@
 import { useLocation } from 'react-router-dom';
 
 import Message from "../layout/Message";
+import Container from "../layout/Container";
+import LinkButton from '../layout/LinkButton';
+import styles from './styles/Project.module.css';
 
 function Projects() {
     const location = useLocation();
@@ -11,12 +14,17 @@ function Projects() {
     }
 
     return (
-        <div>
-            <h1>Meus projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus projetos</h1>
+                <a href='#'>Novo Projeto</a>
+                <LinkButton to="/newproject" text="Criar projeto" />
+            </div>
             {message && <Message type="success" message={message} />}
-        </div>
-
-    )
+            <Container customClass="start">
+                <p>Projetos...</p>
+            </Container>
+        </div>)
 }
 
 export default Projects;
