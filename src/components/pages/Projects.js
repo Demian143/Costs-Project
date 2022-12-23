381cbe049ced907df535ffbca1cab81c56030aff
Demian143 <patrickxavier140@gmail.com>
2022-12-23 15:02:02 -0300

    aula 28

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,11 +1,18 @@
 import { useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 import Message from "../layout/Message";
 import Container from "../layout/Container";
 import LinkButton from '../layout/LinkButton';
+import ProjectCart from '../project/ProjectCard';
+import Loading from '../layout/Loading';
+
 import styles from './styles/Project.module.css';
 
 function Projects() {
+    const [projects, setProjects] = useState([]);
+    const [removeLoading, setRemoveLoading] = useState(false);
+
     const location = useLocation();
     let message = '';
 
@@ -13,16 +20,47 @@ function Projects() {
         message = location.state.message;
     }
 
+    useEffect(() => {
+        setTimeout(
+            () => {
+                fetch('http://localhost:5000/projects',
+                    {
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    })
+                    .then((response) => response.json())
+                    .then((data) => {
+                        setProjects(data);
+                        setRemoveLoading(true);
+                    })
+                    .catch((error) => console.log(error));
+            }, 300)
+    }, [])
+
     return (
         <div className={styles.project_container}>
             <div className={styles.title_container}>
                 <h1>Meus projetos</h1>
-                <a href='#'>Novo Projeto</a>
                 <LinkButton to="/newproject" text="Criar projeto" />
             </div>
             {message && <Message type="success" message={message} />}
             <Container customClass="start">
-                <p>Projetos...</p>
+                {
+                    projects.length > 0 &&
+                    projects.map((project) => (
+                        <ProjectCart
+                            id={project.id}
+                            name={project.name}
+                            budget={project.orçamento}
+                            category={project.category.name}
+                            key={project.id} />
+                    ))}
+                {!removeLoading && <Loading />}
+                {removeLoading && projects.length === 0 && (
+                    <p>Não há projetos cadastrados!</p>
+                )}
             </Container>
         </div>)
 }
