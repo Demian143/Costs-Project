6ea2605caecb426f481352b070b658ee3b6461ce
Demian143 <patrickxavier140@gmail.com>
2023-01-03 14:43:34 -0300

    Aula 29

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -12,6 +12,7 @@ import styles from './styles/Project.module.css';
 function Projects() {
     const [projects, setProjects] = useState([]);
     const [removeLoading, setRemoveLoading] = useState(false);
+    const [projectMessage, setProjectMessage] = useState('');
 
     const location = useLocation();
     let message = '';
@@ -39,6 +40,24 @@ function Projects() {
             }, 300)
     }, [])
 
+    function removeProject(id) {
+        fetch(`http://localhost:5000/projects/${id}`,
+            {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then((response) => response.json())
+            .then(() => {
+                setProjects(
+                    projects.filter(((project) => project.id !== id)
+                    ));
+                setProjectMessage('Projeto removido com sucesso.');
+            })
+            .catch((error) => console.log(error))
+    }
+
     return (
         <div className={styles.project_container}>
             <div className={styles.title_container}>
@@ -46,6 +65,7 @@ function Projects() {
                 <LinkButton to="/newproject" text="Criar projeto" />
             </div>
             {message && <Message type="success" message={message} />}
+            {projectMessage && <Message type="success" message={projectMessage} />}
             <Container customClass="start">
                 {
                     projects.length > 0 &&
@@ -55,7 +75,8 @@ function Projects() {
                             name={project.name}
                             budget={project.orçamento}
                             category={project.category.name}
-                            key={project.id} />
+                            key={project.id}
+                            handleRemove={removeProject} />
                     ))}
                 {!removeLoading && <Loading />}
                 {removeLoading && projects.length === 0 && (
