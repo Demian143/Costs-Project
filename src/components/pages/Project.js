819acac21a3f3a4639da5f731f60a9db86526672
Demian143 <patrickxavier140@gmail.com>
2023-01-09 21:20:03 -0300

    Aula 34

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,3 +1,5 @@
+import { parse, v4 as uuidv4 } from 'uuid';
+
 import styles from './styles/Project.module.css';
 
 import { useParams } from 'react-router-dom';
@@ -7,11 +9,13 @@ import Loading from '../layout/Loading';
 import Container from '../layout/Container';
 import Message from '../layout/Message';
 import ProjectForm from '../project/ProjectForm';
+import ServiceForm from '../services/ServiceForm';
 
 function Project() {
     const { id } = useParams();
     const [project, setProject] = useState([]);
     const [showProjectForm, setShowProjectForm] = useState(false);
+    const [showServiceForm, setShowServiceForm] = useState(false);
     const [message, setMessage] = useState();
     const [type, setType] = useState();
 
@@ -21,12 +25,13 @@ function Project() {
                 method: 'GET',
                 headers: { 'Content-Type': 'application/json' }
             }
-            ).then(
-                (response) => response.json()
-            ).then((data) => {
-                setProject(data)
-            }
-            ).catch((err) => { console.log(err) });
+            )
+                .then(
+                    (response) => response.json())
+                .then((data) => {
+                    setProject(data)
+                })
+                .catch((err) => { console.log(err) });
         }, 3000)
     }, [id]);
 
@@ -58,8 +63,42 @@ function Project() {
             );
     }
 
+    function createService(project) {
+        const lastService = project.services[project.services.length - 1];
+        lastService.id = uuidv4();
+
+        const lastServiceCost = lastService.cost
+        const newCost = parseFloat(project.cost) + parseFloat(lastServiceCost)
+
+        if (newCost > parseFloat(project.orçamento)) {
+
+            setMessage('Orçamento ultrapaçado, verifique o valor do serviço');
+            setType('error');
+            project.services.pop();
+
+            return false;
+        }
+
+        project.cost = newCost;
+
+        fetch(`http://localhost:5000/projects/${project.id}`, {
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(project)
+
+        })
+            .then((response) => response.json())
+            .then((data) => { console.log(data) })
+            .catch(err => console.log(err));
+    }
+
     function toggleProjectForm() {
         setShowProjectForm(!showProjectForm);
+    }
+
+
+    function toggleServiceForm() {
+        setShowServiceForm(!showServiceForm);
     }
 
     return (
@@ -94,6 +133,24 @@ function Project() {
                                 </div>
                             )}
                         </div>
+                        <div className={styles.service_form_container}>
+                            <h2>Adicione um serviço:</h2>
+                            <button className={styles.btn} onClick={toggleServiceForm}>
+                                {!showServiceForm ? 'Editar Projeto' : 'Fechar'}
+                            </button>
+                            <div className={styles.project_info}>
+                                {showServiceForm &&
+                                    <ServiceForm
+                                        handleSubmit={createService}
+                                        btnText='Adicionar Serviço'
+                                        projectData={project}
+                                    />}
+                            </div>
+                        </div>
+                        <h2>Serviços</h2>
+                        <Container customClass='start'>
+                            <p>Items de serviço</p>
+                        </Container>
                     </Container>
                 </div>)
                 :
