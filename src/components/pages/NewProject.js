47452884d2beb66e21da16984433f270cefb75ef
Demian143 <patrickxavier140@gmail.com>
2023-04-08 22:15:58 -0300

    Removing fetch operations in json-server and using local storage as a fake db

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,32 +1,59 @@
 import { useNavigate } from "react-router-dom";
-import styles from './styles/NewProject.module.css';
-import ProjectForm from '../project/ProjectForm';
+import styles from "./styles/NewProject.module.css";
+import ProjectForm from "../project/ProjectForm";
 
 function NewProject() {
-    const history = useNavigate();
+  const history = useNavigate();
 
-    function createPost(project) {
-        // Inicializar o cost e serviços
-        project.cost = 0;
-        project.services = [];
+  function createPost(project) {
+    // Inicializar o cost e serviços
+    project.cost = 0;
+    project.services = [];
 
-        fetch('http://localhost:5000/projects', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(project)
-        })
-            .then((response) => response.json())
-            .then((data) => { history('/projects', { state: { message: 'Projeto criado com sucesso.' } }) })
-            .catch((err) => { console.log(err) });
+    // Set the project obj in the local storage and redirect to /projects
+    // Todo: Add project obj to a list intead of only one obj
+    if (localStorage.getItem("projectList")) {
+      const projectList = JSON.parse(localStorage.getItem("projectList"));
+
+      // Just add to a pre exitent list
+      projectList.push(project);
+      localStorage.setItem("projectList", JSON.stringify(projectList));
+
+      history("/projects", {
+        state: { message: "Projeto criado com sucesso." },
+      });
+    } else {
+      // Create new list
+      localStorage.setItem("projectList", JSON.stringify([project]));
+
+      history("/projects", {
+        state: { message: "Projeto criado com sucesso." },
+      });
     }
 
-    return (
-        <div className={styles.newproject_container} >
-            <h1>Criar Projeto</h1>
-            <p>Crie seu projeto para depois adicionar os serviços.</p>
-            <ProjectForm handleSubmit={createPost} btnText="Criar projeto" />
-        </div>
-    )
+    // fetch("http://localhost:5000/projects", {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/json" },
+    //   body: JSON.stringify(project),
+    // })
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     history("/projects", {
+    //       state: { message: "Projeto criado com sucesso." },
+    //     });
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+  }
+
+  return (
+    <div className={styles.newproject_container}>
+      <h1>Criar Projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços.</p>
+      <ProjectForm handleSubmit={createPost} btnText="Criar projeto" />
+    </div>
+  );
 }
 
 export default NewProject;
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,89 +1,76 @@
-import { useLocation } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 import Message from "../layout/Message";
 import Container from "../layout/Container";
-import LinkButton from '../layout/LinkButton';
-import ProjectCart from '../project/ProjectCard';
-import Loading from '../layout/Loading';
+import LinkButton from "../layout/LinkButton";
+import ProjectCart from "../project/ProjectCard";
+import Loading from "../layout/Loading";
 
-import styles from './styles/Projects.module.css';
+import styles from "./styles/Projects.module.css";
 
 function Projects() {
-    const [projects, setProjects] = useState([]);
-    const [removeLoading, setRemoveLoading] = useState(false);
-    const [projectMessage, setProjectMessage] = useState('');
+  const [projects, setProjects] = useState([]);
+  const [removeLoading, setRemoveLoading] = useState(false);
+  const [projectMessage, setProjectMessage] = useState("");
 
-    const location = useLocation();
-    let message = '';
+  const location = useLocation();
+  let message = "";
 
-    if (location.state) {
-        message = location.state.message;
-    }
+  if (location.state) {
+    message = location.state.message;
+  }
 
-    useEffect(() => {
-        setTimeout(
-            () => {
-                fetch('http://localhost:5000/projects',
-                    {
-                        method: 'GET',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        }
-                    })
-                    .then((response) => response.json())
-                    .then((data) => {
-                        setProjects(data);
-                        setRemoveLoading(true);
-                    })
-                    .catch((error) => console.log(error));
-            }, 300)
-    }, [])
+  // Get the obj in local storage
+  useEffect(() => {
+    const projectsData = localStorage.getItem("projectList");
 
-    function removeProject(id) {
-        fetch(`http://localhost:5000/projects/${id}`,
-            {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-            .then((response) => response.json())
-            .then(() => {
-                setProjects(
-                    projects.filter(((project) => project.id !== id)
-                    ));
-                setProjectMessage('Projeto removido com sucesso.');
-            })
-            .catch((error) => console.log(error))
-    }
+    setProjects(JSON.parse(projectsData));
+    setRemoveLoading(true);
+  }, []);
 
-    return (
-        <div className={styles.project_container}>
-            <div className={styles.title_container}>
-                <h1>Meus projetos</h1>
-                <LinkButton to="/newproject" text="Criar projeto" />
-            </div>
-            {message && <Message type="success" message={message} />}
-            {projectMessage && <Message type="success" message={projectMessage} />}
-            <Container customClass="start">
-                {
-                    projects.length > 0 &&
-                    projects.map((project) => (
-                        <ProjectCart
-                            id={project.id}
-                            name={project.name}
-                            budget={project.orçamento}
-                            category={project.category.name}
-                            key={project.id}
-                            handleRemove={removeProject} />
-                    ))}
-                {!removeLoading && <Loading />}
-                {removeLoading && projects.length === 0 && (
-                    <p>Não há projetos cadastrados!</p>
-                )}
-            </Container>
-        </div>)
+  function removeProject(id) {
+    fetch(`http://localhost:5000/projects/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then(() => {
+        setProjects(projects.filter((project) => project.id !== id));
+        setProjectMessage("Projeto removido com sucesso.");
+      })
+      .catch((error) => console.log(error));
+  }
+
+  return (
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus projetos</h1>
+        <LinkButton to="/newproject" text="Criar projeto" />
+      </div>
+      {message && <Message type="success" message={message} />}
+      {projectMessage && <Message type="success" message={projectMessage} />}
+      <Container customClass="start">
+        {projects.length > 0 &&
+          projects.map((project, index) => (
+            <ProjectCart
+              id={project.id}
+              name={project.name}
+              budget={project.orçamento}
+              category={project.category.name}
+              key={index}
+              handleRemove={removeProject}
+            />
+          ))}
+        {!removeLoading && <Loading />}
+        {removeLoading && projects.length === 0 && (
+          <p>Não há projetos cadastrados!</p>
+        )}
+      </Container>
+    </div>
+  );
 }
 
 export default Projects;
